52b017bedffb1089cf83b84c60e86cee1f5076b5
Prity Sarkar <99pritys@gmail.com>
2026-03-06 19:48:13 +0530

    Delete app directory

--- app/config/cloudinary.js
+++
@@ -1,7 +0,0 @@
-const cloudinary = require('cloudinary').v2;
-
-cloudinary.config({
-  url: process.env.CLOUDINARY_URL,
-});
-
-module.exports = cloudinary;
--- app/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET_KEY, {
-    expiresIn: "1h"
-  });
-};
-
-module.exports = generateToken;
